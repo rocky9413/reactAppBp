891ad2509257a3d2c7ddf9ab6a6418c9179cdc4a
rocky9413 <liangwen511@gmail.com>
2020-05-29 23:40:05 -0400

    modelControllers

--- server/routes/api/apiController.js
+++ server/routes/api/apiController.js
@@ -2,82 +2,13 @@ import fs from 'fs';
 import path from 'path';
 import mongoose from 'mongoose';
 
+import { modelController } from '../modelController';
 import { Pokemons } from './apiModel';
 
-export const getPokemons = (req, res, next) => {
-  const pokemons = JSON.parse(
-    fs.readFileSync(
-      path.resolve(__dirname, '../../data/pokemons.json'),
-      'UTF-8'
-    )
-  );
-
-  if (!pokemons) {
-    return next({
-      log: 'ERROR: Error getting data from pokemons.json file',
-      message: {
-        err: 'Error in getPokemons. Check server logs.'
-      }
-    });
-  }
-
-  mongoose.connection.db.listCollections().toArray((err, data) => {
-    const dbColl = []; // store db collections
-    for (let i = 0; i < data.length; i++) {
-      dbColl.push(data[i].name);
-    }
-
-    // make sure collection doesn't exist before create new collection
-    if (!dbColl.includes('pokemons')) {
-      Pokemons.collection.insertMany([...pokemons], {
-        ordered: true
-      });
-    }
-  });
-
-  res.locals.characters = pokemons;
-  next();
-};
-
-export const getByName = (req, res, next) => {
-  const { pokeName } = req.body;
-
-  Pokemons.findOne({ name: pokeName }, function(err, onePoke) {
-    if (err) {
-      return res.send({ err: 'error in getByName' });
-    }
-    if (onePoke) res.locals.onePoke = onePoke;
-    else {
-      res.locals.onePoke = { notExist: 'pokemon name not exist!!' };
-    }
-
-    return next();
-  });
-};
-
-export const getById = (req, res, next) => {
-  const id = req.params.id === ':id' ? req.body.pokeId : req.params.id;
-
-  if (Number(id) < 1 || Number(id) > 150 || id.length > 3) {
-    res.locals.onePoke = { notExist: 'please enter correct id!!' };
-    return next();
-  }
-
-  Pokemons.findOne({ id }, function(err, onePoke) {
-    if (err) {
-      return res.send({ err: 'error in getById' });
-    }
-    // console.log('onepoke === ', onePoke);
-    if (onePoke.id) res.locals.onePoke = onePoke;
-    else {
-      res.locals.onePoke = { notExist: 'pokemon id not exist!!' };
-    }
-    return next();
-  });
-};
+export default modelController(Pokemons);
 
 // MIDDLEWARE TO GET FAVORITE CHARACTERS HERE
-export const getFavs = (req, res, next) => {
+const getFavs = (req, res, next) => {
   const results = JSON.parse(
     fs.readFileSync(path.resolve(__dirname, '../data/favs.json'), 'UTF-8')
   );
@@ -86,7 +17,7 @@ export const getFavs = (req, res, next) => {
 };
 
 // ADD MIDDLEWARE TO ADD A FAVORITE CHARACTER HERE
-export const addFav = (req, res, next) => {
+const addFav = (req, res, next) => {
   if (!(res.locals.favs || typeof res.locals.favs === 'object')) {
     return next({
       log: 'addFavs: ERROR: Invalid data on res.locals object.',
@@ -109,7 +40,7 @@ export const addFav = (req, res, next) => {
 };
 
 // MIDDLEWARE TO REMOVE A CHARACTER FROM FAVORITES HERE
-export const removeFav = (req, res, next) => {
+const removeFav = (req, res, next) => {
   const favsObj = res.locals.favs;
   const { id } = req.params;
   delete favsObj[id];
@@ -119,4 +50,63 @@ export const removeFav = (req, res, next) => {
     'UTF-8'
   );
   next();
+};
+
+// old function below
+const getPokemons = (req, res, next) => {
+  const pokemons = JSON.parse(
+    fs.readFileSync(
+      path.resolve(__dirname, '../../data/pokemons.json'),
+      'UTF-8'
+    )
+  );
+
+  if (!pokemons) {
+    return next({
+      log: 'ERROR: Error getting data from pokemons.json file',
+      message: {
+        err: 'Error in readFile'
+      }
+    });
+  }
+
+  // check mongodb db collections
+  mongoose.connection.db.listCollections().toArray((err, data) => {
+    const dbColl = []; // store db collections
+    for (let i = 0; i < data.length; i++) {
+      dbColl.push(data[i].name);
+    }
+
+    // make sure collection doesn't exist before create new collection
+    if (!dbColl.includes('pokemons')) {
+      Pokemons.collection.insertMany([...pokemons], {
+        ordered: true
+      });
+    }
+  });
+
+  res.locals.characters = pokemons;
+  next();
+};
+
+// Old ready to delete
+const getById = (req, res, next) => {
+  const id = req.params.id === ':id' ? req.body.pokeId : req.params.id;
+
+  if (Number(id) < 1 || Number(id) > 150 || id.length > 3) {
+    res.locals.onePoke = { notExist: 'please enter correct id!!' };
+    return next();
+  }
+
+  Pokemons.findOne({ id }, function(err, onePoke) {
+    if (err) {
+      return res.send({ err: 'error in getById' });
+    }
+    // console.log('onepoke === ', onePoke);
+    if (onePoke.id) res.locals.onePoke = onePoke;
+    else {
+      res.locals.onePoke = { notExist: 'pokemon id not exist!!' };
+    }
+    return next();
+  });
 };
--- server/routes/api/apiRouter.js
+++ server/routes/api/apiRouter.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
-import { getPokemons, getById, getByName } from './apiController';
+// import { getPokemons, getById, getByName } from './apiController';
+import controllers from './apiController';
 // import addFavRouter from '../favs.js';
 
 // const moreCharactersRouter = require('./characters.js');
@@ -11,22 +12,11 @@ const router = Router();
 
 // router.use('/characters', moreCharactersRouter);
 
-const allChars = (req, res) => {
-  res.status(200).send({
-    characters: res.locals.characters,
-    favs: res.locals.favs
-  });
-};
+router.route('/allPoke').get(controllers.checkNgetAll);
 
-const oneChar = (req, res) => {
-  res.status(200).send({ onePoke: res.locals.onePoke });
-};
+router.route('/name').get(controllers.getOneByName);
 
-router.route('/allPoke').get(getPokemons, allChars);
-
-router.route('/name').get(getPokemons, getByName, oneChar);
-
-router.route('/:id').get(getPokemons, getById, oneChar);
+router.route('/:id').get(controllers.getOneById);
 
 // router.use('/allPoke', getPokemons, (req, res) => {
 //   res.status(200).send({
